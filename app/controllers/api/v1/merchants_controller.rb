f51027a7eb1383a49cd642e0c67c0441f0cceee8
lingtran <lxiutran@gmail.com>
2016-05-31 17:42:31 -0600

    Implement Show on Record and Single Finders
    Create routes for api/v1/merchants/:id and api/v1/merchants/find?parameters single finders endpoints

--- app/controllers/api/v1/merchants_controller.rb
+++ app/controllers/api/v1/merchants_controller.rb
@@ -4,4 +4,17 @@ class Api::V1::MerchantsController < Api::ApiController
   def index
     respond_with Merchant.all
   end
+
+  def show
+    respond_with Merchant.find_by(id: params[:id])
+  end
+
+  def find
+    respond_with Merchant.find_by(merchant_params)
+  end
+
+  private
+  def merchant_params
+    params.permit(:name, :created_at, :updated_at)
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,9 @@
 Rails.application.routes.draw do
   namespace :api, defaults: { format: :json } do
     namespace :v1 do
-      resources :merchants, only: [:index]
+      resources :merchants, only: [:index, :show] do
+        get 'find', on: :collection
+      end
     end
   end
 end
